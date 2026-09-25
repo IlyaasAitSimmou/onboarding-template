5f2b000a69fc72a04d7fb1ee7c23fbc0751f72be
IlyaasAitSimmou <yaseenaitsimmou@gmail.com>
2026-09-25 09:28:14 -0400

    pragma omp parallel for actually added

--- src/submission.hpp
+++ src/submission.hpp
@@ -62,7 +62,8 @@ void apply_stencil(const Grid &old_grid, Grid &new_grid)
     new_grid(rows - 1, j) = old_grid(rows - 1, j);
   }
 
-  for (std::size_t i{1}; i < rows - 1; ++i)
+#pragma omp parallel for
+  for (std::size_t i = 1; i < rows - 1; ++i)
   {
     for (std::size_t j{1}; j < cols - 1; ++j)
     {
